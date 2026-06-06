fb56b63fc524ed4f60eaf073df238940d6225acf
ekhong00-create <ekhong00@gmail.com>
2026-06-06 17:39:27 +0900

    Rename Stop 02 to '그라운드 고척 먹자골목' in timeline, markers and sections

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ if (typeof Kakao !== 'undefined' && KAKAO_APP_KEY && KAKAO_APP_KEY !== 'YOUR_KAK
 const MAP_COORDS = [
   { name: '구일역 2번 출구', lat: 37.49567, lng: 126.86789 },
   { name: 'STOP 01 서울아트책보고', lat: 37.49897, lng: 126.86721 },
-  { name: 'STOP 02 그라운드 고척 (포장)', lat: 37.50003, lng: 126.86714 },
+  { name: 'STOP 02 그라운드 고척 먹자골목', lat: 37.50003, lng: 126.86714 },
   { name: '고척 스카이돔', lat: 37.4982, lng: 126.8671 },
   { name: 'STOP 03 뒷풀이', lat: 37.4998, lng: 126.8660 }
 ];

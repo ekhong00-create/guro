f6e1aa056da0be89de79d0cd43319889170a8258
ekhong00-create <ekhong00@gmail.com>
2026-06-07 23:51:40 +0900

    Update Kakao Map API marker label for STEP 02 to '그라운드 고척 - 포장 & 안주 맛집'

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ if (typeof Kakao !== 'undefined' && KAKAO_APP_KEY && KAKAO_APP_KEY !== 'YOUR_KAK
 const MAP_COORDS = [
   { name: '구일역 2번 출구', lat: 37.49567, lng: 126.86789 },
   { name: 'STEP 01 서울아트책보고', lat: 37.49897, lng: 126.86721 },
-  { name: 'STEP 02 그라운드 고척 - 간편 포장 맛집', lat: 37.50003, lng: 126.86714 },
+  { name: 'STEP 02 그라운드 고척 - 포장 & 안주 맛집', lat: 37.50003, lng: 126.86714 },
   { name: '고척돔', lat: 37.4982, lng: 126.8671 },
   { name: 'STEP 04 그라운드 고척 - 뒷풀이', lat: 37.5005101, lng: 126.8665257 }
 ];
